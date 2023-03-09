23affd968485133a9e670975e914dbbd8520ccfe
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-09 14:34:14 +0330

    creating Symbol model with migrations

--- Asymbol/models.py
+++ Asymbol/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Symbol(models.Model):
+    name = models.CharField(max_length=150, null=True, blank=True)
+    final_price = models.CharField(max_length=150, null=True, blank=True)
+    volume = models.CharField(max_length=150, null=True, blank=True)
+    price = models.CharField(max_length=150, null=True, blank=True)
