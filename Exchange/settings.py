f99015f5fca6d888b51b78f27d3594b33bb8f5d8
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-09 13:36:23 +0330

    configuring celery.py

--- Exchange/settings.py
+++ Exchange/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Celery settings
+# https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
+
