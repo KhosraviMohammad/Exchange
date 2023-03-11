704397470c8b650c93ebf4886e1b25d25617661e
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-11 14:54:26 +0330

    updated create_sqlite_engine task func

--- Asymbol/tasks.py
+++ Asymbol/tasks.py
@@ -1,18 +1,26 @@
+import datetime
+
 from celery import shared_task
 import requests
 import time
 import io
 import pandas
+from Asymbol.models import Symbol
 
+from utils.engine import create_sqlite_engine
+
+ENGINE = create_sqlite_engine(path='D:\programming\python\django\projects\Exchange\db.sqlite3')
 
 @shared_task
 def get_data_from_tsetmc_com():
-    i = 0
     while True:
-        i += 1
         response = requests.get('http://members.tsetmc.com/tsev2/excel/MarketWatchPlus.aspx?d=0')
         xlsx = io.BytesIO(response.content)
         df = pandas.read_excel(xlsx)
-        df = df.iloc[2:, [0, 9]]
-        df.to_excel(f"output{i}.xlsx")
-        time.sleep(10)
+        df = df.iloc[2:, [0,3, 11, 19]]
+        df.columns = ['name', 'volume', 'final_price', 'price']
+        df['stored_date'] = datetime.datetime.now()
+        table_name = Symbol.objects.model._meta.db_table
+        df.to_sql('{0}'.format(table_name), con=ENGINE, if_exists='append', index=False)
+        time.sleep(5)
+
