ee272623a2527cd28002bedb22f746f925ccf70c
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-15 09:58:47 +0330

    correcting tasks in task.py

--- Asymbol/tasks.py
+++ Asymbol/tasks.py
@@ -13,24 +13,24 @@ from Asymbol.models import Symbol
 
 from utils.engine import create_sqlite_engine
 from utils.task_functions import calculate_time_axis
+import finpy_tse as fpy
+
 
 ENGINE = create_sqlite_engine(path='D:\programming\python\django\projects\Exchange\db.sqlite3')
 
 
 @shared_task
 def get_data_from_tsetmc_com():
-    while True:
-        response = requests.get('http://members.tsetmc.com/tsev2/excel/MarketWatchPlus.aspx?d=0')
-        xlsx = io.BytesIO(response.content)
-        df = pandas.read_excel(xlsx)
-        df = df.iloc[2:, [0,3, 11, 19]]
-        df.columns = ['name', 'volume', 'final_price', 'price']
-        df['stored_date'] = datetime.datetime.now()
-        table_name = Symbol.objects.model._meta.db_table
-        df.to_sql('{0}'.format(table_name), con=ENGINE, if_exists='append', index=False)
-        time.sleep(5)
+    response = requests.get('http://members.tsetmc.com/tsev2/excel/MarketWatchPlus.aspx?d=0')
+    xlsx = io.BytesIO(response.content)
+    df = pandas.read_excel(xlsx)
+    df = df.iloc[2:, [0,3, 11, 19]]
+    df.columns = ['name', 'volume', 'final_price', 'price']
+    df['stored_date'] = datetime.datetime.now()
+    table_name = Symbol.objects.model._meta.db_table
+    df.to_sql('{0}'.format(table_name), con=ENGINE, if_exists='append', index=False)
 
-
+@shared_task
 def calculate_slope():
     # calculate x axis
     time_axis_point_list = calculate_time_axis()
@@ -49,4 +49,4 @@ def calculate_slope():
     slope = slope.reshape(len(slope), 1)
     data_frame['slope'] = slope
 
-    return data_frame
+    return 1
