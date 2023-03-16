4303687bc6f8472f609c4e9a40cd35457e29bb9b
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-16 14:37:08 +0330

    final_price_change field is added to symbol model

--- Asymbol/models.py
+++ Asymbol/models.py
@@ -13,3 +13,4 @@ class Symbol(models.Model):
     volume = models.CharField(max_length=30, null=True, blank=True)
     price = models.CharField(max_length=30, null=True, blank=True)
     stored_date = models.DateTimeField(auto_now=True)
+    final_price_change = models.CharField(max_length=30, null=True, blank=True)
