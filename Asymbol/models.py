79b488b4e71ef5873fb8dd95dcd1143401aa188d
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-16 12:23:07 +0330

    some fields are changed and added to symbol model

--- Asymbol/models.py
+++ Asymbol/models.py
@@ -4,8 +4,12 @@ from django.db import models
 
 
 class Symbol(models.Model):
-    name = models.CharField(max_length=150, null=True, blank=True)
-    final_price = models.CharField(max_length=150, null=True, blank=True)
-    volume = models.CharField(max_length=150, null=True, blank=True)
-    price = models.CharField(max_length=150, null=True, blank=True)
+    symbol_name = models.CharField(max_length=30, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    sector = models.CharField(max_length=50, null=True, blank=True)
+    market = models.CharField(max_length=50, null=True, blank=True)
+    final_price = models.CharField(max_length=30, null=True, blank=True)
+    time = models.TimeField(null=True, blank=True)
+    volume = models.CharField(max_length=30, null=True, blank=True)
+    price = models.CharField(max_length=30, null=True, blank=True)
     stored_date = models.DateTimeField(auto_now=True)
