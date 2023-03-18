44d8375f21feb52573fa576ad8d55c5ae5bac492
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-18 23:49:47 +0330

    manage_tasks is corrected

--- Asymbol/tasks.py
+++ Asymbol/tasks.py
@@ -1,22 +1,18 @@
 import datetime
-from builtins import enumerate
-
-from celery import shared_task
-import requests
 import time
-import io
-import pandas
-import numpy
-from django.db.models import Count, F
-from Exchange.celery import app
-from Asymbol.models import Symbol
 
-from utils.engine import create_sqlite_engine
-from utils.task_functions import calculate_time_axis_point, calculate_final_price_changes_from_Final_percent_and_Final_amount, \
-    convert_date_time_str_list_to_date_time_obj_list
 import finpy_tse as fpy
-
+import numpy
+import pandas
 from django.db import connection
+from django.db.models import Min, Max
+
+from Asymbol.models import Symbol, Slope
+from Exchange.celery import app
+from utils.engine import create_sqlite_engine
+from utils.task_functions import calculate_time_axis_point, \
+    calculate_final_price_changes_from_Final_percent_and_Final_amount, \
+    convert_date_time_str_list_to_date_time_obj_list
 
 ENGINE = create_sqlite_engine(path='D:\programming\python\django\projects\Exchange\db.sqlite3')
 
@@ -105,14 +101,24 @@ def calculate_slope():
 
 @app.task()
 def manage_tasks():
+    symbol_table_name = Symbol.objects.model._meta.db_table
+    slope_table_name = Symbol.objects.model._meta.db_table
     time_start = datetime.datetime.now()
-    is_task_done = True
     while True:
-        if is_task_done:
-            task_tsetmc = get_data_from_tsetmc_com.delay()
-        is_task_done = task_tsetmc.ready()
+        data_from_tsetmc_com = get_data_from_tsetmc_com()
+        data_from_tsetmc_com.to_sql('{0}'.format(symbol_table_name), con=ENGINE, if_exists='append', index=True)
+        time.sleep(3)
         time_delta = datetime.datetime.now() - time_start
-        if time_delta.total_seconds() >= 300:
-            calculate_slope.delay()
+        if time_delta.total_seconds() >= 300: # 5 min * 60 sec = 300 sec
+            today_date_time = datetime.datetime.today()
+            today_date = datetime.datetime(year=today_date_time.year, month=today_date_time.month, day=today_date_time.day)
+            slope_qs = Slope.objects.filter(from_date__gte=today_date)
+            if slope_qs.exists():
+                from_date = slope_qs.aggregate(Max('stored_date'))
+                to_date = from_date + datetime.timedelta(seconds=300)
+            else:
+                from_date = Symbol.objects.filter().aggregate(today_smallest_date=Min('stored_date'))['today_smallest_date']
+                to_date = from_date + datetime.timedelta(seconds=300)
+            slope_data_frame = calculate_new_slope(from_date=from_date, to_date=to_date)
+            slope_data_frame.to_sql('{0}'.format(slope_table_name), con=ENGINE, if_exists='append', index=True)
             time_start = datetime.datetime.now()
-
