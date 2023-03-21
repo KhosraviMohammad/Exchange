d36df1a22d02cd49c724c1001939c76af98eebc2
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-21 11:19:41 +0330

    SlopeView is created and added to url

--- Asymbol/urls.py
+++ Asymbol/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from Asymbol.views import SlopeView
+
+router = routers.SimpleRouter()
+
+
+router.register('slope', SlopeView)
+
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
--- Asymbol/views.py
+++ Asymbol/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets, mixins
+
+
+from Asymbol.models import Slope
+from Asymbol.serializers import SlopeSerializer
 # Create your views here.
+
+
+class SlopeView(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = Slope.objects.all()
+    serializer_class = SlopeSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
