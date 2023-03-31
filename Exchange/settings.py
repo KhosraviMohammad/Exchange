cff21b1a88c6b6713afb97370d1b1b58c6b3adfe
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-03-31 13:42:18 +0430

    database is switched to postgresql

--- Exchange/settings.py
+++ Exchange/settings.py
@@ -80,13 +80,23 @@ WSGI_APPLICATION = 'Exchange.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'amir',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
