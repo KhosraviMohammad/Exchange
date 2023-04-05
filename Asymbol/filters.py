5c4d2d601d7e5cd9ca7990a374d0748234e0b043
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-04-05 16:43:33 +0430

    the way of filtering on from_date is changed SlopeFilter

--- Asymbol/filters.py
+++ Asymbol/filters.py
@@ -8,9 +8,9 @@ from Asymbol.models import Slope
 class SlopeFilter(django_filters.FilterSet):
     value_from = django_filters.NumberFilter(field_name='value', lookup_expr='gte')
     value_to = django_filters.NumberFilter(field_name='value', lookup_expr='lte')
+    from_date_start = django_filters.DateTimeFilter(field_name='from_date', lookup_expr='gte')
+    from_date_end = django_filters.DateTimeFilter(field_name='from_date', lookup_expr='lte')
 
     class Meta:
         model = Slope
-        fields = ['value_from', 'value_to', 'from_date']
-
-
+        fields = ['value_from', 'value_to', 'from_date_start', 'from_date_end']
