414dacf0335c85b7a704910883e6f80f7e42d6fb
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-06 16:03:41 +0430

    getting value from environment in settings.py

--- Exchange/settings.py
+++ Exchange/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*0v^^f9@_t&mk*d(nyeuqth_=0adg143)(7@sd)xz11%62xfws'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-*0v^^f9@_t&mk*d(nyeuqth_=0adg143)(7@sd)xz11%62xfws')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True),
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', '*')]
 
 
 # Application definition
@@ -148,6 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "static/"
+
+
+MEDIA_ROOT = BASE_DIR / "media/"
+
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
