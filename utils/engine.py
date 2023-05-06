16a5259aeb7cf449e03cc3f51ef315541ae4f042
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-07 00:05:43 +0430

    getting info of database from settings.py for engine

--- utils/engine.py
+++ utils/engine.py
@@ -1,6 +1,12 @@
+from django.conf import settings
 import sqlalchemy
 
 
-def create_sqlite_engine():
-    engine = sqlalchemy.create_engine(fr'postgresql+psycopg2://postgres:amir@localhost:5432/test')
+def create_postgres_engine():
+    data_base = settings.DATABASES['default']
+    NAME = data_base.get('NAME')
+    USER = data_base.get('USER')
+    PASSWORD = data_base.get('PASSWORD')
+    HOST = data_base.get('HOST')
+    engine = sqlalchemy.create_engine(fr'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:5432/{NAME}')
     return engine
