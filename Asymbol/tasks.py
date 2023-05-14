23d0f1c918dd7986772b715aa331375c6ce301bf
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-14 14:26:20 +0330

    increased time sleep in task.get_data_from_tsetmc_com

--- Asymbol/tasks.py
+++ Asymbol/tasks.py
@@ -41,7 +41,7 @@ def get_data_from_tsetmc_com():
             cleaned_market_watch_data_frame.to_sql('{0}'.format(symbol_table_name), con=ENGINE, if_exists='append', index=True)
         except ValueError:
             print('no respond from tsetmc_com')
-        time.sleep(8)
+        time.sleep(10)
 
 
 @app.task()
