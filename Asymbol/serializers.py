8509c73648c9e4e9b670d26517b667f34da5acc2
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-20 14:19:18 +0330

    updated time zone in SlopeSerializer

--- Asymbol/serializers.py
+++ Asymbol/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from Asymbol.models import Slope
-
+import pytz
 
 class SlopeSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.IntegerField(read_only=True)
     symbol_name = serializers.CharField(read_only=True)
     value = serializers.CharField(read_only=True)
-    from_date = serializers.DateTimeField(read_only=True)
-    to_date = serializers.DateTimeField(read_only=True)
+    from_date = serializers.DateTimeField(read_only=True, default_timezone=pytz.timezone('UTC'))
+    to_date = serializers.DateTimeField(read_only=True, default_timezone=pytz.timezone('UTC'))
 
     class Meta:
         model = Slope
