2cb5d7136acaa33b9cbca114e554c97f94cc06bb
KhosraviMohammad <mammad.khosravi.78@gamile.com>
2023-05-28 13:28:12 +0330

    SlopeView.quryset is changed

--- Asymbol/views.py
+++ Asymbol/views.py
@@ -1,8 +1,6 @@
-from django.db.models import F
+from django.db.models import F, Q
 
 from rest_framework import viewsets, mixins
-
-
 
 from django_filters import rest_framework as filters
 
@@ -10,11 +8,13 @@ from Asymbol.models import Slope
 from Asymbol.serializers import SlopeSerializer
 from Asymbol.filters import SlopeFilter
 
+
 # Create your views here.
 
 
 class SlopeView(viewsets.GenericViewSet, mixins.ListModelMixin):
-    queryset = Slope.objects.all().order_by(F('from_date').desc(), F('value').desc(nulls_last=True), 'symbol_name')
+    queryset = Slope.objects.all().filter(Q(value__isnull=False) & ~Q(value=0)).order_by(F('from_date').desc(), F('value').desc(),
+                                                                        'symbol_name')
     serializer_class = SlopeSerializer
     filter_backends = [filters.DjangoFilterBackend]
     filterset_class = SlopeFilter
